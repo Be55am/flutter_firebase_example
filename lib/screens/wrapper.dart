98c6728e01edfa00d26a11dd22a6f7abb2f27692
Bessam <sahlibessam@gmail.com>
2021-12-30 14:00:22 +0100

    basic app structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_flutter/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // return home or auth
+    return Home();
+  }
+}
+
