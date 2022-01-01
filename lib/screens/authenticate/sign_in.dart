2b17d83d31190d0a642f1387770584dfdea0ef04
Bessam <sahlibessam@gmail.com>
2022-01-01 13:09:33 +0100

    create Sign in form

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:firebase_flutter/services/auth.dart';
 import 'package:flutter/material.dart';
 
@@ -11,8 +9,11 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
-
   final AuthService _authService = AuthService();
+
+  //text field state
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
@@ -25,17 +26,60 @@ class _SignInState extends State<SignIn> {
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: ElevatedButton(
-          child: Text('Sign In Anonymously'),
-          onPressed: () async {
-            dynamic res = await _authService.signInAnon();
-            if(res == null){
-              print('Error signing in');
-            } else {
-              print('Signed in');
-              // print(res);
-            }
-          },
+        child: Form(
+          child: Column(children: [
+            SizedBox.fromSize(
+              size: Size.fromHeight(20.0),
+            ),
+            TextFormField(
+              onChanged: (val) {
+                //Do something with the user input.
+                setState(() {
+                  email = val;
+                });
+              },
+              decoration: InputDecoration(
+                hintText: 'Email',
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            SizedBox.fromSize(
+              size: Size.fromHeight(20.0),
+            ),
+            TextFormField(
+              obscureText: true,
+              onChanged: (val) {
+                //Do something with the user input.
+                setState(() {
+                  password = val;
+                });
+              },
+              decoration: InputDecoration(
+                hintText: 'Password',
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+            SizedBox.fromSize(
+              size: Size.fromHeight(20.0),
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.pink),
+              ),
+                onPressed: () async {
+                print(email + ' ' + password);
+                },
+                child: Text('Sign In'),
+            ),
+          ]),
         ),
       ),
     );
